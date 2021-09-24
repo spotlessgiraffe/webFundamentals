ffebf82d4fbffabd1d9d50f328b1cca2c5cdf17b
Heather <situh@hawaii.edu>
2021-09-23 22:46:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeLogout(element) {
+    element.innerText = "Logout";
+}
+
+function removeBtn(element) {
+    element.remove();
+}
+
+function alertLikes() {
+    alert(`Ninja was liked`);
+}
+
+// Sources:
+// 1. https://login.codingdojo.com/m/283/8970/60833 [onclick]
+// 2. https://login.codingdojo.com/m/283/8970/60834 [Javascript - "this"]
